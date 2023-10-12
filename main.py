5f940bbe6d4e534e9523db223a26c20c970f1419
mamasagung <65334552+mamasagung@users.noreply.github.com>
2023-10-12 17:25:01 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+"""
+This is a telegram chat bot with ChatGPT under the hood!
+
+1. Paste your Telegram Bot Token and ChatGPT API Key
+2. Run the code and open the bot in telegram.
+  - wanna run 24/7 -> upload to a server and run there
+"""
+
+import openai
+from aiogram import Bot, Dispatcher, executor, types
+
+# create bot with @BotFather and copy Token
+bot = Bot(token='6344110183:AAE0m-jQrySeXXPcGpa1blhLRCq-7qfYY-w')
+dp = Dispatcher(bot)
+
+api_key = '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' # https://platform.openai.com/account/api-keys
+openai.api_key = api_key
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def welcome(message: types.Message):
+    await message.reply('Hey! This Chat Bot is using ChatGPT and was developed by kit4py. Enjoy!')
+
+
+@dp.message_handler()
+async def gpt(message: types.Message):
+    response = openai.Completion.create(
+        model="text-davinci-003", # text-davinci-003 text-ada-001 -> select model to use 
+        prompt=message.text,
+        temperature=0.5,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+    await message.reply(response.choices[0].text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
